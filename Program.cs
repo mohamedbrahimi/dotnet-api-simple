ae54cd0bac4247fe06aaedd2704e03a029625526
mohamedbrahimi <mohkriss3@gmail.com>
2023-01-08 20:04:42 +0100

    defined service with interface.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using webapi_try;
+using webapi_try.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// App services 
+builder.Services.AddScoped<IPaperService, PaperService>();
 
 var app = builder.Build();
 
