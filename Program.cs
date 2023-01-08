fde7ea362af10d0351014956c3f49eff685f0973
mohamedbrahimi <mohkriss3@gmail.com>
2023-01-08 21:54:49 +0100

    use singleton instead if scoped.

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // App services 
-builder.Services.AddScoped<IPaperService, PaperService>();
+builder.Services.AddSingleton<IPaperService, PaperService>();
 
 var app = builder.Build();
 
