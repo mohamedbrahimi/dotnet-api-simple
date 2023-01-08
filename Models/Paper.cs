3d1e9db14a27ca5ed9f7825abd39ee73ee30004b
mohamedbrahimi <mohkriss3@gmail.com>
2023-01-08 20:04:00 +0100

    model

---
+++ Models/Paper.cs
@@ -0,0 +1,9 @@
+namespace webapi_try.Models;
+
+public class Paper
+{
+    public int Id { get; set; }
+    public string Path { get; set; }
+    public string? Name { get; set; }
+    public string? Dir { get; set; }
+}
