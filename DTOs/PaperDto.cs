f8540d6be143143a966e6bdb19f2e33b1684b7c2
mohamedbrahimi <mohkriss3@gmail.com>
2023-01-10 11:50:05 +0100

    paper DTO.

---
+++ DTOs/PaperDto.cs
@@ -0,0 +1,9 @@
+namespace webapi_try.DTOs;
+
+public class PaperDto
+{
+    public int Id { get; set; }
+    public string Path { get; set; }
+    public string? Name { get; set; }
+    public string? Dir { get; set; }
+}
