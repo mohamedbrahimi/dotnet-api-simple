0252dc903c0cd1e56f01a706d1e662137f4c15c4
mohamedbrahimi <mohkriss3@gmail.com>
2023-01-12 11:40:50 +0100

    env config service.

---
+++ Tools/ConstantsEnv.cs
@@ -0,0 +1,8 @@
+namespace webapi_try.Tools;
+
+public class ConstantsEnv
+{
+    public static string DB_CONNECTION_STRING = "DB_CONNECTION_STRING";
+    public static string API_KEY = "API_KEY";
+    public static string SECRET_KEY = "SECRET_KEY";
+}
